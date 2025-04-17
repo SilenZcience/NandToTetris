69ae1e8d9c0f3e17884aa1fa232c9cb8eccee511
Silas Kraume <silas.kraume1552@gmail.com>
2025-04-17 19:30:26 +0200

    implemented project 4

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -3,9 +3,68 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/4/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, 
+// i.e. writes "black" in every pixel. When no key is pressed,
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// initiate the @pos symbol to the screen base address minus 1
+// (because we will increment it before using it every iteration)
+    @SCREEN
+    D=A
+    @pos
+    M=D-1
+
+(AWAIT)
+
+    @KBD
+    D=M
+    @BLACKEN // if a key is pressed, go to BLACKEN
+    D;JGT
+    @CLEAR   // else go to CLEAR
+    0;JMP
+
+(BLACKEN)
+
+    @24575 // == 16384[SCREEN BASE ADDRESS] + (256[ROWS] * 512[COLS]) / 16 - 1 == the max address of the screen
+    D=A
+    @pos
+    D=D-M
+    @AWAIT
+    D;JLE // == JEQ, if the screen is fully black (we reached the last screen address), go back to AWAIT
+
+    // first increment, then write the pixel
+
+    @pos
+    M=M+1 // increment position to the next pixel
+
+    A=M
+    M=-1  // color the pixel black
+
+    // continue blackening the screen because it is more efficient and the task did not specify interactivity during screen changes
+    // instead we could jump back to AWAIT and slowly lower or raise the black 'curtain'
+    @BLACKEN
+    0;JMP
+
+(CLEAR)
+
+    @SCREEN
+    D=A
+    @pos
+    D=M-D
+    @AWAIT
+    D;JLT  // if the screen is fully cleared (we exceeded the first screen address), go back to AWAIT
+
+    // first write the pixel, then decrement
+
+    @pos
+    A=M
+    M=0   // color the pixel white
+
+    @pos
+    M=M-1 // decrement position to the previous pixel
+
+    // continue clearing the screen because it is more efficient and the task did not specify interactivity during screen changes
+    // instead we could jump back to AWAIT and slowly lower or raise the black 'curtain'
+    @CLEAR
+    0;JMP
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+    @2
+    M=0 // clean R2
+
+    @i // create iterative 'variable'
+    M=0
+
+(LOOP) // add R0 to R2, R1 times
+
+// -------------------------------------------------------------------
+// check break condition:
+    @1
+    D=M
+
+    @i
+    M=M+1 // increment iterative 'variable'
+
+    D=D-M // calculate difference
+    @END
+    D;JLT // break if iterative 'variable' exceeds R1
+// -------------------------------------------------------------------
+
+// -------------------------------------------------------------------
+// repetitive addition (without use of the symbol table):
+    @0
+    D=M // get R0 value
+
+    @2
+    M=D+M // simply accumulate the value of R0 in R2 directly
+// -------------------------------------------------------------------
+
+// loop back
+    @LOOP
+    0;JMP
+
+(END)
+// nothing to do here, sum is already accumulated in R2
